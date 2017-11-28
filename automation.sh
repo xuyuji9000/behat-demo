4c9f4144adb8a722beae536bce3b48c40500cffa
Karl Xu <karl.yogi.xu@gmail.com>
2017-11-28 19:38:55 +0800

    init behat

--- automation.sh
+++ automation.sh
@@ -8,7 +8,11 @@ case "$1" in
     "run" )
         docker run --rm -it -v "$PWD":/app -w /app behat-demo /bin/sh
         ;;
+    "test" )
+        docker run --rm -it -v "$PWD":/app -w /app behat-demo /bin/sh -c "./vendor/bin/behat"
+        ;;
     * )
-        echo "${White}run${NC}: run php in docker"
+        echo "${White}run${NC}: run php in docker\n"
+        echo "${White}test${NC}: test with behat\n"
 
 esac
